9bbcc2f49b024eaeef3c97eec64d537eb160d8fd
Vardan2009 <70532109+Vardan2009@users.noreply.github.com>
2025-11-17 10:11:25 +0400

    Basic file reading

--- asm/src/main.c
+++ asm/src/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    printf("Hello, Assembler!\n");
+    if (argc != 2) {
+        fprintf(stderr, "p8asm: usage: %s <.asm path>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *fptr;
+    char buffer[512];
+
+    fptr = fopen(argv[1], "r");
+
+    if (fptr == NULL) {
+        perror("p8asm");
+        return 1;
+    }
+
+    while (fgets(buffer, sizeof(buffer), fptr) != NULL) printf("%s", buffer);
+
+    fclose(fptr);
+
     return 0;
 }
